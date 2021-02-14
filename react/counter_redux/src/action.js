2f39e0c38fc3a131b0e18d1d5b50791b643d02a3
b0nanze <iumkhaev@gmail.com>
2021-02-14 15:32:48 +0300

    userNum

---
+++ react/counter_redux/src/action.js
@@ -0,0 +1,24 @@
+export function plusOne(){
+    return {
+        type: "plus"
+    }
+}
+
+export function minusOne(){
+    return {
+        type: "minus"
+    }
+}
+
+export function resetCounter (){
+    return {
+        type: "reset"
+    }
+}
+
+export function userNum(number){
+    return {
+        type: "amount",
+        payload: number
+    }
+}
